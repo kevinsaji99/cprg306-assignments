87f70b71976e61da9d7fc6b2528bdd35eee12148
kevinsaji99 <kevinsaji@live.com>
2026-09-17 18:48:15 -0600

    Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name:Kevin Saji</p>
+            <Link href="https://github.com/kevinsaji99/cprg306-assignments">My GitHub Repository</Link>
+        </div>
+    );
+}
